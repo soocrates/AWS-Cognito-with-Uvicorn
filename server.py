6f273323ca1ea62c2d899993c59c392f04e5f241
VtechRoShan <rooshan.vtech@gmail.com>
2024-02-08 11:33:14 +0545

    Fix Assigning response data before accessing value

--- server.py
+++ server.py
@@ -58,15 +58,15 @@ def read_authenticated(code: Optional[str] = None):
             response = requests.post(token_endpoint, data=data, headers=headers)
             # Check response status
             if response.status_code == 200:
+                response_data = response.json()
                 if not response_data["token_type"] == 'Bearer':
                     # Parse the JSON response
-                    response_data = response.json()
                     response.set_cookie(key="id_token", value=response_data["id_token"], httponly=True, secure=True)
                     response.set_cookie(key="access_token", value=response_data["access_token"], httponly=True, secure=True)
                     response.set_cookie(key="refresh_token", value=response_data["refresh_token"], httponly=True, secure=True)
                     response.set_cookie(key="expires_in", value=response_data["expires_in"], httponly=True, secure=True)
 
-                    return response_data
+                    return { "response": response_data, "code": code}
                 else:
                     raise HTTPException(status_code=403, detail="Wrong authentication method") 
             else:
@@ -81,6 +81,6 @@ def read_authenticated(code: Optional[str] = None):
                 "Response": cognito_idp_response.text
             }
             
-@app.get("/authenticated/{username}")
+@app.get("/authenticated/{username}", Depends())
 def get_user_name():
     return {"Hello": "World"}
